d2dc20c144b4b86b2385982e341105be43dacdc5
Sanjay Saatyaki <Saatyaki.Sanjay@in.bosch.com>
2022-02-07 16:49:02 +0530

    Changes for ccn

--- check_limits.py
+++ check_limits.py
@@ -1,16 +1,20 @@
 def isInRange(value, min_value, max_value=None):
-  if value < min_value or value > max_value:
+  if value > min_value and value < max_value:
     return True
+  return False
+
+def isGreaterThanMin(value, min_value):
+  if value > min_value:
+    return False
+  return True
 
 def battery_is_ok(temperature, soc, charge_rate):
-  if isInRange(temperature,0,45) or isInRange(soc,20,80):
-    print('Temperature or soc is out of range!')
-    return False
-  if not isInRange(charge_rate,0.8):
-    print('Charge rate is out of range!')
-    return False
 
-  return True
+  isTemperatureOk = isInRange(temperature,0,45)
+  isSocOk = isInRange(soc,20,80)
+  isChargeOk = isGreaterThanMin(charge_rate,0.8)
+
+  return isTemperatureOk and isSocOk and isChargeOk
 
 
 if __name__ == '__main__':
