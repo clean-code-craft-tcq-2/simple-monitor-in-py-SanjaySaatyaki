63a3846032ac8938bc6b588d8d1d8735e8caa1be
Sanjay Saatyaki <Saatyaki.Sanjay@in.bosch.com>
2022-02-13 22:16:28 +0530

    Fix for CCN and test cases

--- check_limits.py
+++ check_limits.py
@@ -1,7 +1,7 @@
 battery_params_range = {
-  'temperature' : {'min':0,'max':45,'tolerance':5},
-  'soc' : {'min':20,'max':80,'tolerance':5},
-  'charge_rate' : {'min':0,'max':0.8,'tolerance':5}
+  'temperature' : {'min':0,'max':45,'tolerance':5,'alert':True},
+  'soc' : {'min':20,'max':80,'tolerance':5,'alert':True},
+  'charge_rate' : {'min':0,'max':0.8,'tolerance':5,'alert':True}
 }
 
 battery_state = {0:"NORMAL",1:"LOW LEVEL",2:"HIGH LEVEL"}
@@ -25,7 +25,7 @@ languages = {
         'HIGH LEVEL BREACHED': 'HOHES LEVEL ERREICHT',
         'LOW LEVEL WARNING':'NIEDRIGES NIVEAU Warnung',
         'HIGH LEVEL WARNING': 'HOHES LEVEL Warnung',
-  }
+        }
 }
 
 
@@ -53,24 +53,26 @@ def get_range(value, min_value, max_value,check):
   else:
     return get_limit(value,min_value,check)
 
-def battery_param_status(params,enable_warning,lang):
+def get_higher_precedence(limit_status,warning_status):
+  if limit_status is not battery_state[0]:
+    return limit_status
+  return warning_status
+
+def battery_param_status(params,lang):
   battery_params = {}
   for param in params:
-    if enable_warning:
-      tolerance = (battery_params_range[param]['max']*battery_params_range[param]['tolerance'])/100
-      status = get_range(params[param],battery_params_range[param]['min']+tolerance,battery_params_range[param]['max']-tolerance,' WARNING')
-    else:
-      status = get_range(params[param],battery_params_range[param]['min'],battery_params_range[param]['max'],' BREACHED')
-    battery_params[param] = status
-
+    limit_status = get_range(params[param],battery_params_range[param]['min'],battery_params_range[param]['max'],' BREACHED')
+    tolerance = (battery_params_range[param]['max']*battery_params_range[param]['tolerance'])/100
+    warning_status = get_range(params[param],battery_params_range[param]['min']+tolerance,battery_params_range[param]['max']-tolerance,' WARNING')
+    battery_params[param] = limit_status
+    if battery_params_range[param]['alert']:
+      battery_params[param] = get_higher_precedence(limit_status,warning_status)
   display_param_status(battery_params,lang)
-
   return battery_params
       
 if __name__ == '__main__':
-  assert(battery_param_status({'temperature':25,'soc':70,'charge_rate':0.1},True,"EN") == {'temperature': 'NORMAL', 'soc': 'NORMAL', 'charge_rate': 'NORMAL'})
-  assert(battery_param_status({'temperature':43,'soc':70,'charge_rate':0.1},True,"EN") == {'temperature': 'HIGH LEVEL WARNING', 'soc': 'NORMAL', 'charge_rate': 'NORMAL'})
-  assert(battery_param_status({'temperature':43,'soc':70,'charge_rate':0.1},False,"EN") == {'temperature': 'NORMAL', 'soc': 'NORMAL', 'charge_rate': 'NORMAL'})
-  assert(battery_param_status({'temperature':50,'soc':85,'charge_rate':0},False,"DE") == {'temperature': 'HIGH LEVEL BREACHED', 'soc': 'HIGH LEVEL BREACHED', 'charge_rate': 'HIGH LEVEL BREACHED'})
+  assert(battery_param_status({'temperature':25,'soc':70,'charge_rate':0.1},"EN") == {'temperature': 'NORMAL', 'soc': 'NORMAL', 'charge_rate': 'NORMAL'})
+  assert(battery_param_status({'temperature':43,'soc':70,'charge_rate':0.1},"EN") == {'temperature': 'HIGH LEVEL WARNING', 'soc': 'NORMAL', 'charge_rate': 'NORMAL'})
+  assert(battery_param_status({'temperature':50,'soc':85,'charge_rate':0},"DE") == {'temperature': 'HIGH LEVEL BREACHED', 'soc': 'HIGH LEVEL BREACHED', 'charge_rate': 'HIGH LEVEL BREACHED'})
   
   assert(convert_to_language(['temperature','LOW LEVEL BREACHED'],"DE") == "Temperatur\tNIEDRIGES NIVEAU ERREICHT")
